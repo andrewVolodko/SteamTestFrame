f8504a62902c28bf5ad87ee788476731560fcd68
Andrew Volodko <andrewvolodko@Andrews-MacBook-Pro.local>
2021-08-25 22:33:01 +0300

    MainPage pageOpened indicator fixed

--- Steam.Core/Pages/MainPage.cs
+++ Steam.Core/Pages/MainPage.cs
@@ -6,13 +6,14 @@ namespace SteamTestFrame.Pages
 {
     public class MainPage : BasePage
     {
+        private static readonly By HomePageBannerBy = By.ClassName("home_page_col_wrapper");
         private static readonly By InstallBtnBy = By.ClassName("header_installsteam_btn_content");
 
         public MainPage(BrowserService browserService) : base(browserService)
         {
         }
 
-        protected override By GetCorrectPageOpenedIndicatorElLocator() => InstallBtnBy;
+        protected override By GetCorrectPageOpenedIndicatorElLocator() => HomePageBannerBy;
 
         public IWebElement GetInstallBtn() => BrowserService.GetDriver().FindElement(InstallBtnBy);
     }
